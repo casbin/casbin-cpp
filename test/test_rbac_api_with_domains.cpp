9a647b47d4b1d56744e877d678b8d1ea8a0421f9
divy9881 <divy9881@gmail.com>
2020-07-24 20:29:50 +0530

    fix: Add smart pointer to the tests.
    Signed-off-by: divy9881 <divy9881@gmail.com>

--- test/test_rbac_api_with_domains.cpp
+++ test/test_rbac_api_with_domains.cpp
@@ -16,81 +16,81 @@ namespace test_rbac_api_with_domains
         public:
 
             TEST_METHOD(TestGetImplicitRolesForDomainUser) {
-                unique_ptr<Enforcer> e = Enforcer::NewEnforcer("../../examples/rbac_with_domains_model.conf", "../../examples/rbac_with_hierarchy_with_domains_policy.csv");
+                Enforcer e = Enforcer("../../examples/rbac_with_domains_model.conf", "../../examples/rbac_with_hierarchy_with_domains_policy.csv");
 
                 // This is only able to retrieve the first level of roles.
-                Assert::IsTrue(ArrayEquals({ "role:global_admin" }, e->GetRolesForUserInDomain("alice", { "domain1" })));
+                Assert::IsTrue(ArrayEquals({ "role:global_admin" }, e.GetRolesForUserInDomain("alice", { "domain1" })));
 
                 // Retrieve all inherit roles. It supports domains as well.
-                Assert::IsTrue(ArrayEquals(vector<string>{"role:global_admin", "role:reader", "role:writer"}, e->GetImplicitRolesForUser("alice", {"domain1"})));
+                Assert::IsTrue(ArrayEquals(vector<string>{"role:global_admin", "role:reader", "role:writer"}, e.GetImplicitRolesForUser("alice", {"domain1"})));
             }
 
             // TestUserAPIWithDomains: Add by Gordon
             TEST_METHOD(TestUserAPIWithDomains) {
-                unique_ptr<Enforcer> e = Enforcer::NewEnforcer("../../examples/rbac_with_domains_model.conf", "../../examples/rbac_with_domains_policy.csv");
+                Enforcer e = Enforcer("../../examples/rbac_with_domains_model.conf", "../../examples/rbac_with_domains_policy.csv");
 
-                Assert::IsTrue(ArrayEquals({ "alice" }, e->GetUsersForRole("admin", { "domain1" })));
-                Assert::IsTrue(ArrayEquals({ "alice" }, e->GetUsersForRoleInDomain("admin", { "domain1" })));
+                Assert::IsTrue(ArrayEquals({ "alice" }, e.GetUsersForRole("admin", { "domain1" })));
+                Assert::IsTrue(ArrayEquals({ "alice" }, e.GetUsersForRoleInDomain("admin", { "domain1" })));
                 
                 try {
-                    e->GetUsersForRole("non_exist", { "domain1" });
+                    e.GetUsersForRole("non_exist", { "domain1" });
                 }
                 catch (CasbinRBACException e) {
                     Assert::IsTrue(true);
                 }
                 try {
-                    e->GetUsersForRoleInDomain("non_exist", { "domain1" });
+                    e.GetUsersForRoleInDomain("non_exist", { "domain1" });
                 }
                 catch (CasbinRBACException e) {
                     Assert::IsTrue(true);
                 }
 
-                Assert::IsTrue(ArrayEquals({ "bob" }, e->GetUsersForRole("admin", { "domain2" })));
-                Assert::IsTrue(ArrayEquals({ "bob" }, e->GetUsersForRoleInDomain("admin", { "domain2" })));
+                Assert::IsTrue(ArrayEquals({ "bob" }, e.GetUsersForRole("admin", { "domain2" })));
+                Assert::IsTrue(ArrayEquals({ "bob" }, e.GetUsersForRoleInDomain("admin", { "domain2" })));
 
                 try {
-                    e->GetUsersForRole("non_exist", { "domain2" });
+                    e.GetUsersForRole("non_exist", { "domain2" });
                 }
                 catch (CasbinRBACException e) {
                     Assert::IsTrue(true);
                 }
                 try {
-                    e->GetUsersForRoleInDomain("non_exist", { "domain2" });
+                    e.GetUsersForRoleInDomain("non_exist", { "domain2" });
                 }
                 catch (CasbinRBACException e) {
                     Assert::IsTrue(true);
                 }
 
-                e->DeleteRoleForUserInDomain("alice", "admin", "domain1");
-                e->AddRoleForUserInDomain("bob", "admin", "domain1");
+                e.DeleteRoleForUserInDomain("alice", "admin", "domain1");
+                e.AddRoleForUserInDomain("bob", "admin", "domain1");
 
-                Assert::IsTrue(ArrayEquals({ "bob" }, e->GetUsersForRole("admin", { "domain1" })));
-                Assert::IsTrue(ArrayEquals({ "bob" }, e->GetUsersForRoleInDomain("admin", { "domain1" })));
+                Assert::IsTrue(ArrayEquals({ "bob" }, e.GetUsersForRole("admin", { "domain1" })));
+                Assert::IsTrue(ArrayEquals({ "bob" }, e.GetUsersForRoleInDomain("admin", { "domain1" })));
 
                 try {
-                    e->GetUsersForRole("non_exist", { "domain1" });
+                    e.GetUsersForRole("non_exist", { "domain1" });
                 }
                 catch (CasbinRBACException e) {
                     Assert::IsTrue(true);
                 }
                 try {
-                    e->GetUsersForRoleInDomain("non_exist", { "domain1" });
+                    e.GetUsersForRoleInDomain("non_exist", { "domain1" });
                 }
                 catch (CasbinRBACException e) {
                     Assert::IsTrue(true);
                 }
 
-                Assert::IsTrue(ArrayEquals({ "bob" }, e->GetUsersForRole("admin", { "domain2" })));
-                Assert::IsTrue(ArrayEquals({ "bob" }, e->GetUsersForRoleInDomain("admin", { "domain2" })));
+                Assert::IsTrue(ArrayEquals({ "bob" }, e.GetUsersForRole("admin", { "domain2" })));
+                Assert::IsTrue(ArrayEquals({ "bob" }, e.GetUsersForRoleInDomain("admin", { "domain2" })));
 
                 try {
-                    e->GetUsersForRole("non_exist", { "domain2" });
+                    e.GetUsersForRole("non_exist", { "domain2" });
                 }
                 catch (CasbinRBACException e) {
                     Assert::IsTrue(true);
                 }
                 try {
-                    e->GetUsersForRoleInDomain("non_exist", { "domain2" });
+                    e.GetUsersForRoleInDomain("non_exist", { "domain2" });
                 }
                 catch (CasbinRBACException e) {
                     Assert::IsTrue(true);
@@ -98,62 +98,62 @@ namespace test_rbac_api_with_domains
             }
 
             TEST_METHOD(TestRoleAPIWithDomains) {
-                unique_ptr<Enforcer> e = Enforcer::NewEnforcer("../../examples/rbac_with_domains_model.conf", "../../examples/rbac_with_domains_policy.csv");
+                Enforcer e = Enforcer("../../examples/rbac_with_domains_model.conf", "../../examples/rbac_with_domains_policy.csv");
                 
-                Assert::IsTrue(ArrayEquals({ "admin" }, e->GetRolesForUser("alice", { "domain1" })));
-                Assert::IsTrue(ArrayEquals({ "admin" }, e->GetRolesForUserInDomain("alice", { "domain1" })));
+                Assert::IsTrue(ArrayEquals({ "admin" }, e.GetRolesForUser("alice", { "domain1" })));
+                Assert::IsTrue(ArrayEquals({ "admin" }, e.GetRolesForUserInDomain("alice", { "domain1" })));
 
-                Assert::IsTrue(ArrayEquals({ }, e->GetRolesForUser("bob", { "domain1" })));
-                Assert::IsTrue(ArrayEquals({ }, e->GetRolesForUserInDomain("bob", { "domain1" })));
+                Assert::IsTrue(ArrayEquals({ }, e.GetRolesForUser("bob", { "domain1" })));
+                Assert::IsTrue(ArrayEquals({ }, e.GetRolesForUserInDomain("bob", { "domain1" })));
 
-                Assert::IsTrue(ArrayEquals({ }, e->GetRolesForUser("admin", { "domain1" })));
-                Assert::IsTrue(ArrayEquals({ }, e->GetRolesForUserInDomain("admin", { "domain1" })));
+                Assert::IsTrue(ArrayEquals({ }, e.GetRolesForUser("admin", { "domain1" })));
+                Assert::IsTrue(ArrayEquals({ }, e.GetRolesForUserInDomain("admin", { "domain1" })));
                 
-                Assert::IsTrue(ArrayEquals({ }, e->GetRolesForUser("non_exist", { "domain1" })));
-                Assert::IsTrue(ArrayEquals({ }, e->GetRolesForUserInDomain("non_exist", { "domain1" })));
+                Assert::IsTrue(ArrayEquals({ }, e.GetRolesForUser("non_exist", { "domain1" })));
+                Assert::IsTrue(ArrayEquals({ }, e.GetRolesForUserInDomain("non_exist", { "domain1" })));
 
-                Assert::IsTrue(ArrayEquals({ }, e->GetRolesForUser("alice", { "domain2" })));
-                Assert::IsTrue(ArrayEquals({ }, e->GetRolesForUserInDomain("alice", { "domain2" })));
+                Assert::IsTrue(ArrayEquals({ }, e.GetRolesForUser("alice", { "domain2" })));
+                Assert::IsTrue(ArrayEquals({ }, e.GetRolesForUserInDomain("alice", { "domain2" })));
 
-                Assert::IsTrue(ArrayEquals({ "admin" }, e->GetRolesForUser("bob", { "domain2" })));
-                Assert::IsTrue(ArrayEquals({ "admin" }, e->GetRolesForUserInDomain("bob", { "domain2" })));
+                Assert::IsTrue(ArrayEquals({ "admin" }, e.GetRolesForUser("bob", { "domain2" })));
+                Assert::IsTrue(ArrayEquals({ "admin" }, e.GetRolesForUserInDomain("bob", { "domain2" })));
 
-                Assert::IsTrue(ArrayEquals({ }, e->GetRolesForUser("admin", { "domain2" })));
-                Assert::IsTrue(ArrayEquals({ }, e->GetRolesForUserInDomain("admin", { "domain2" })));
+                Assert::IsTrue(ArrayEquals({ }, e.GetRolesForUser("admin", { "domain2" })));
+                Assert::IsTrue(ArrayEquals({ }, e.GetRolesForUserInDomain("admin", { "domain2" })));
 
-                Assert::IsTrue(ArrayEquals({ }, e->GetRolesForUser("non_exist", { "domain2" })));
-                Assert::IsTrue(ArrayEquals({ }, e->GetRolesForUserInDomain("non_exist", { "domain2" })));
+                Assert::IsTrue(ArrayEquals({ }, e.GetRolesForUser("non_exist", { "domain2" })));
+                Assert::IsTrue(ArrayEquals({ }, e.GetRolesForUserInDomain("non_exist", { "domain2" })));
 
-                e->DeleteRoleForUserInDomain("alice", "admin", "domain1");
-                e->AddRoleForUserInDomain("bob", "admin", "domain1");
+                e.DeleteRoleForUserInDomain("alice", "admin", "domain1");
+                e.AddRoleForUserInDomain("bob", "admin", "domain1");
 
-                Assert::IsTrue(ArrayEquals({ }, e->GetRolesForUser("alice", { "domain1" })));
-                Assert::IsTrue(ArrayEquals({ }, e->GetRolesForUserInDomain("alice", { "domain1" })));
+                Assert::IsTrue(ArrayEquals({ }, e.GetRolesForUser("alice", { "domain1" })));
+                Assert::IsTrue(ArrayEquals({ }, e.GetRolesForUserInDomain("alice", { "domain1" })));
 
-                Assert::IsTrue(ArrayEquals({ "admin" }, e->GetRolesForUser("bob", { "domain1" })));
-                Assert::IsTrue(ArrayEquals({ "admin" }, e->GetRolesForUserInDomain("bob", { "domain1" })));
+                Assert::IsTrue(ArrayEquals({ "admin" }, e.GetRolesForUser("bob", { "domain1" })));
+                Assert::IsTrue(ArrayEquals({ "admin" }, e.GetRolesForUserInDomain("bob", { "domain1" })));
 
-                Assert::IsTrue(ArrayEquals({ }, e->GetRolesForUser("admin", { "domain1" })));
-                Assert::IsTrue(ArrayEquals({ }, e->GetRolesForUserInDomain("admin", { "domain1" })));
+                Assert::IsTrue(ArrayEquals({ }, e.GetRolesForUser("admin", { "domain1" })));
+                Assert::IsTrue(ArrayEquals({ }, e.GetRolesForUserInDomain("admin", { "domain1" })));
 
-                Assert::IsTrue(ArrayEquals({ }, e->GetRolesForUser("non_exist", { "domain1" })));
-                Assert::IsTrue(ArrayEquals({ }, e->GetRolesForUserInDomain("non_exist", { "domain1" })));
+                Assert::IsTrue(ArrayEquals({ }, e.GetRolesForUser("non_exist", { "domain1" })));
+                Assert::IsTrue(ArrayEquals({ }, e.GetRolesForUserInDomain("non_exist", { "domain1" })));
 
-                Assert::IsTrue(ArrayEquals({ }, e->GetRolesForUser("alice", { "domain2" })));
-                Assert::IsTrue(ArrayEquals({ }, e->GetRolesForUserInDomain("alice", { "domain2" })));
+                Assert::IsTrue(ArrayEquals({ }, e.GetRolesForUser("alice", { "domain2" })));
+                Assert::IsTrue(ArrayEquals({ }, e.GetRolesForUserInDomain("alice", { "domain2" })));
 
-                Assert::IsTrue(ArrayEquals({ "admin" }, e->GetRolesForUser("bob", { "domain2" })));
-                Assert::IsTrue(ArrayEquals({ "admin" }, e->GetRolesForUserInDomain("bob", { "domain2" })));
+                Assert::IsTrue(ArrayEquals({ "admin" }, e.GetRolesForUser("bob", { "domain2" })));
+                Assert::IsTrue(ArrayEquals({ "admin" }, e.GetRolesForUserInDomain("bob", { "domain2" })));
 
-                Assert::IsTrue(ArrayEquals({ }, e->GetRolesForUser("admin", { "domain2" })));
-                Assert::IsTrue(ArrayEquals({ }, e->GetRolesForUserInDomain("admin", { "domain2" })));
+                Assert::IsTrue(ArrayEquals({ }, e.GetRolesForUser("admin", { "domain2" })));
+                Assert::IsTrue(ArrayEquals({ }, e.GetRolesForUserInDomain("admin", { "domain2" })));
 
-                Assert::IsTrue(ArrayEquals({ }, e->GetRolesForUser("non_exist", { "domain2" })));
-                Assert::IsTrue(ArrayEquals({ }, e->GetRolesForUserInDomain("non_exist", { "domain2" })));
+                Assert::IsTrue(ArrayEquals({ }, e.GetRolesForUser("non_exist", { "domain2" })));
+                Assert::IsTrue(ArrayEquals({ }, e.GetRolesForUserInDomain("non_exist", { "domain2" })));
             }
 
-            void TestGetPermissionsInDomain(unique_ptr<Enforcer>& e, string name, string domain, vector<vector<string>> res) {
-                vector<vector<string>> my_res = e->GetPermissionsForUserInDomain(name, { domain });
+            void TestGetPermissionsInDomain(Enforcer e, string name, string domain, vector<vector<string>> res) {
+                vector<vector<string>> my_res = e.GetPermissionsForUserInDomain(name, { domain });
 
                 int count = 0;
                 for (int i = 0; i < my_res.size(); i++) {
@@ -169,7 +169,7 @@ namespace test_rbac_api_with_domains
             }
 
             TEST_METHOD(TestPermissionAPIInDomain) {
-                unique_ptr<Enforcer> e = Enforcer::NewEnforcer("../../examples/rbac_with_domains_model.conf", "../../examples/rbac_with_domains_policy.csv");
+                Enforcer e = Enforcer("../../examples/rbac_with_domains_model.conf", "../../examples/rbac_with_domains_policy.csv");
 
                 TestGetPermissionsInDomain(e, "alice", "domain1", {});
                 TestGetPermissionsInDomain(e, "bob", "domain1", {});
