a73a9efad2872cc3094e91854c70f3a7707af8ce
divy9881 <divy9881@gmail.com>
2021-01-07 15:52:30 +0530

    fix: file naming convention and fix vs filters and bootstrap file opening process.
    Signed-off-by: divy9881 <divy9881@gmail.com>

--- casbin/ip_parser/exception/parser_exception.cpp
+++ casbin/ip_parser/exception/parser_exception.cpp
@@ -2,7 +2,7 @@
 
 #include "pch.h"
 
-#include "./parserException.h"
+#include "./parser_exception.h"
 
 ParserException :: ParserException(string error_message){
     this->error_message = error_message;
