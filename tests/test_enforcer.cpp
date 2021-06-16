bbfdd66229fff5b6446de4bfa5e930b4cd889d6f
Yash Pandey (YP) <yash.btech.cs19@iiitranchi.ac.in>
2021-06-16 10:26:05 +0530

    feat: Added more tests
    Signed-off-by: Yash Pandey (YP) <yash.btech.cs19@iiitranchi.ac.in>

--- tests/test_enforcer.cpp
+++ tests/test_enforcer.cpp
@@ -1,3 +1,21 @@
+/*
+* Copyright 2020 The casbin Authors. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+* This is a test file showcasing the workflow of casbin::Enforcer
+*/
+
 #include <gtest/gtest.h>
 #include <casbin/casbin.h>
 
@@ -29,4 +47,49 @@ TEST(TestEnforcer, TestThreeParams) {
     ASSERT_EQ(e.Enforce({ "bob", "data1", "write" }), false);
     ASSERT_EQ(e.Enforce({ "bob", "data2", "read" }), false);
     ASSERT_EQ(e.Enforce({ "bob", "data2", "write" }), true);
+}
+
+TEST(TestEnforcer, TestVectorParams) {
+    std::string model = "../../examples/basic_model_without_spaces.conf";
+    std::string policy = "../../examples/basic_policy.csv";
+    casbin::Enforcer e(model, policy);
+
+    ASSERT_EQ(e.Enforce({ "alice", "data1", "read" }), true);
+    ASSERT_EQ(e.Enforce({ "alice", "data1", "write" }), false);
+    ASSERT_EQ(e.Enforce({ "alice", "data2", "read" }), false);
+    ASSERT_EQ(e.Enforce({ "alice", "data2", "write" }), false);
+    ASSERT_EQ(e.Enforce({ "bob", "data1", "read" }), false);
+    ASSERT_EQ(e.Enforce({ "bob", "data1", "write" }), false);
+    ASSERT_EQ(e.Enforce({ "bob", "data2", "read" }), false);
+    ASSERT_EQ(e.Enforce({ "bob", "data2", "write" }), true);
+}
+
+TEST(TestEnforcer, TestMapParams) {
+    std::string model = "../../examples/basic_model_without_spaces.conf";
+    std::string policy = "../../examples/basic_policy.csv";
+    casbin::Enforcer e(model, policy);
+
+    std::unordered_map<std::string, std::string> params = {{"sub", "alice"}, {"obj", "data1"}, {"act", "read"}};
+    ASSERT_EQ(e.Enforce(params), true);
+
+    params = { {"sub","alice"},{"obj","data1"},{"act","write"} };
+    ASSERT_EQ(e.Enforce(params), false);
+
+    params = { {"sub","alice"},{"obj","data2"},{"act","read"} };
+    ASSERT_EQ(e.Enforce(params), false);
+
+    params = { {"sub","alice"},{"obj","data2"},{"act","write"} };
+    ASSERT_EQ(e.Enforce(params), false);
+
+    params = { {"sub","bob"},{"obj","data1"},{"act","read"} };
+    ASSERT_EQ(e.Enforce(params), false);
+
+    params = { {"sub","bob"},{"obj","data1"},{"act","write"} };
+    ASSERT_EQ(e.Enforce(params), false);
+
+    params = { {"sub","bob"},{"obj","data2"},{"act","read"} };
+    ASSERT_EQ(e.Enforce(params), false);
+
+    params = { {"sub","bob"},{"obj","data2"},{"act","write"} };
+    ASSERT_EQ(e.Enforce(params), true);
 }
