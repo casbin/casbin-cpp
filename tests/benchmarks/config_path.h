17cc825047e51634c695f85f7a2b6ea908dd5fb4
Yash Pandey (YP) <yash.btech.cs19@iiitranchi.ac.in>
2021-07-03 18:41:34 +0530

    test: Added benchmarks for CachedEnforcer
    Signed-off-by: Yash Pandey (YP) <yash.btech.cs19@iiitranchi.ac.in>

---
+++ tests/benchmarks/config_path.h
@@ -0,0 +1,35 @@
+/*
+* Copyright 2021 The casbin Authors. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+* This file contains the path to various config resources within casbin
+*/
+
+#include <string>
+
+static const std::string relative_path = "../../../";
+static const std::string basic_model_path = relative_path + "examples/basic_model.conf";
+static const std::string basic_policy_path = relative_path + "examples/basic_policy.csv";
+static const std::string rbac_model_path = relative_path + "examples/rbac_model.conf";
+static const std::string rbac_policy_path = relative_path + "examples/rbac_policy.csv";
+static const std::string rbac_with_resource_roles_model_path = relative_path + "examples/rbac_with_resource_roles_model.conf";
+static const std::string rbac_with_resource_roles_policy_path = relative_path + "examples/rbac_with_resource_roles_policy.csv";
+static const std::string rbac_with_domains_model_path = relative_path + "examples/rbac_with_domains_model.conf";
+static const std::string rbac_with_domains_policy_path = relative_path + "examples/rbac_with_domains_policy.csv";
+static const std::string keymatch_model_path = relative_path + "examples/keymatch_model.conf";
+static const std::string keymatch_policy_path = relative_path + "examples/keymatch_policy.csv";
+static const std::string rbac_with_deny_model_path = relative_path + "examples/rbac_with_deny_model.conf";
+static const std::string rbac_with_deny_policy_path = relative_path + "examples/rbac_with_deny_policy.csv";
+static const std::string priority_model_path = relative_path + "examples/priority_model.conf";
+static const std::string priority_policy_path = relative_path + "examples/priority_policy.csv";
