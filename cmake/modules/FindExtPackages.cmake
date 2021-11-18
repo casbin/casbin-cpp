7a28a0f056076e546e7e4ea958c6d969264b41bd
Stonex <43725202+sheny1xuan@users.noreply.github.com>
2021-11-19 01:15:32 +0800

    fix: update json version to 3.10.1 (#169)
    Signed-off-by: stonex <1479765922@qq.com>

--- cmake/modules/FindExtPackages.cmake
+++ cmake/modules/FindExtPackages.cmake
@@ -24,7 +24,7 @@ set(CMAKE_FIND_PACKAGE_NO_SYSTEM_PACKAGE_REGISTRY ON CACHE BOOL
 ###############################################################################
 ### Packages and versions ###
 
-find_package(json 3.7.3 REQUIRED)
+find_package(json 3.10.1 REQUIRED)
 
 if(CASBIN_BUILD_TEST)
     # googletest
